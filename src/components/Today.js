84518936d5b19ccb2781ce83b51e3cb025365de0
vickyrockstar <116479689+vickyrockstar@users.noreply.github.com>
2023-03-23 15:11:13 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -3,8 +3,9 @@ import ListRender from "./ListRender";
 
 const Today = (props) => {
   const date = new Date();
-  const today = props.list.filter((item) => {
-    return item.date === date.toLocaleDateString("en-US");
+  
+  const today = props.list.filter((itm) => {
+    return itm.date === date.toLocaleDateString("en-US");
   });
   
   return (
